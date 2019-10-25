3651eddea20457a9051851a841a479ecf9865d41
Michael Schletz <schletz@spengergasse.at>
2019-10-25 20:31:07 +0200

    Commit 20191025

---
+++ Asciidoc/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace LinqUebung1.App
+{
+    class Program
+    {
+        // tag::mainMethod[]
+        static void Main(string[] args)
+        {
+           Console.WriteLine("Hello!");
+        }
+        // end::mainMethod[]
+    }
+}
+
